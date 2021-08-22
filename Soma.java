ab66010508beea6ac9908e44363950eaa7364337
xKaue007 <kaueoliveira92@gmail.com>
2021-08-22 19:53:58 +0000

    Soma

---
+++ Soma.java
@@ -0,0 +1,9 @@
+public class Soma{
+    public static void main (String args[]){
+        int soma=0;
+        for(int i=1; i<=1000; i++){
+            soma=soma+i;
+        }
+         System.out.println("Soma: " +soma);
+    }
+}
